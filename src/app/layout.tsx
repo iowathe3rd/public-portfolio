350fb4265fef517d63fa060a3e9acab6cf7c1706
Baurzhan Beglerov <bbeglerov@icloud.com>
2024-06-22 06:11:39 +0500

    feat: Add @vercel/analytics package for website analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import { PrismicPreview } from "@prismicio/next";
 import { createClient, repositoryName } from "@/prismicio";
+import { Analytics } from "@vercel/analytics/react"
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -36,6 +37,7 @@ export default function RootLayout({
         <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
         <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
         <Footer />
+        <Analytics/>
         <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
