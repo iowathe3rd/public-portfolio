4229b089cc1665fe84c5c795aed63a43f93bdf8f
Baurzhan Beglerov <bbeglerov@icloud.com>
2024-06-22 06:44:55 +0500

    feat: Update generateMetadata function to include Google site verification
    - Modify the generateMetadata function in the Page component to include the "google-site-verification" property in the returned metadata object.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,13 @@ export default async function Page() {
   return <SliceZone slices={page.data.slices} components={components} />;
 }
 
-export async function generateMetadata(): Promise<Metadata> {
+export async function generateMetadata(): Promise<Metadata & {"google-site-verification" : string}> {
   const client = createClient();
   const page = await client.getSingle("homepage");
 
   return {
     title: page.data.meta_title,
     description: page.data.meta_description,
+    "google-site-verification": "57wGkbDdwQoPxv-LZ-LSKPoVfdbJ7FcmwqnErS0OtvA",
   };
 }
